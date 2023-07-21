7d279a66901b7d75b1f171190b9ae28548be9445
wangliguang <wangliguang@huoban.com>
2023-07-21 11:16:53 +0800

    集成包进去

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { openAliScan} from '@huoban-rn/ali-mpaas'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Button title='XXXX' onPress={() => {
+        openAliScan(true)
+      }}/>
+
+      
+
       <StatusBar style="auto" />
     </View>
   );
